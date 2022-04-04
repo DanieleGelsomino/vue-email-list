771f4ed0bd1e4c4dec5e9b20858c3e56f9331b33
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-04-04 15:27:03 +0200

    exercise completed

--- js/script.js
+++ js/script.js
@@ -11,4 +11,18 @@ console.log("JS OK!");
 
 const app = new Vue({
   el: "#app",
+  data: {
+    emailList: [],
+  },
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          const result = response.data;
+          this.emailList.push(result);
+          console.log(result);
+        });
+    }
+  },
 });
